e1870b6388b8d9f26c2881d082e2a7149604b105
fitrahizulfalaq <fitrahizulfalaq@gmail.com>
2021-09-18 22:57:25 +0700

    just anggota

--- application/controllers/Anggota.php
+++ application/controllers/Anggota.php
@@ -22,7 +22,7 @@ class Anggota extends CI_Controller {
 		$data['menu'] = "Data Anggota";
 		$data['header_script'] = "anggota_header";
 		$data['footer_script'] = "anggota_footer";
-		$this->db->where('tipe_user <','2');
+		$this->db->where('tipe_user','1');
 		$data['row'] = $this->anggota_m->getPendaftar($this->session->tipe_user,$this->session->komisariat_id,$this->session->rayon_id);
 		$this->templateadmin->load('template/dashboard','anggota/data',$data);
 	}
