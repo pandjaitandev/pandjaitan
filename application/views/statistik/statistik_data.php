8a9ffdc40ba8bd4940807dc62dee2d7bb04ee991
fitrahizulfalaq <fitrahizulfalaq@gmail.com>
2021-10-05 21:03:45 +0700

    statst on komisariat account

--- application/views/statistik/statistik_data.php
+++ application/views/statistik/statistik_data.php
@@ -49,6 +49,31 @@
         <!-- interactive chart -->
         <div class="card card-primary card-outline">
           <div class="card-header">
+            <h3 class="card-title">Berdasarkan Sebaran Per Tahun</h3>
+
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+              </button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+            </div>
+          </div>
+          <div class="card-body">
+            <canvas id="perkembanganChart" style="min-height: 250px; height: 250px; max-width: 100%;"></canvas>
+          </div>
+          <!-- /.card-body-->
+        </div>
+        <!-- /.card -->
+
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+
+    <div class="row">
+      <div class="col-12">
+        <!-- interactive chart -->
+        <div class="card card-primary card-outline">
+          <div class="card-header">
             <h3 class="card-title">Berdasarkan Sebaran Per Provinsi</h3>
 
             <div class="card-tools">
